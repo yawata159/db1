b4fa21c2300fc72e9920814cf80696c134438e3b
yawata159 <seijiyawata@gmail.com>
2016-10-18 22:22:46 -0400

    Finish get_averages

--- get_averages.py
+++ get_averages.py
@@ -3,24 +3,28 @@ import sqlite3
 f="discobandit.db"
 db = sqlite3.connect(f)
 c = db.cursor()    
+d = db.cursor()
 
 # look up the grades for each student
 def getGrades(student):
     query = "SELECT mark FROM courses, students WHERE courses.id = students.id and name = ?"
-    c.execute(query, (student,) )
-    return [elem[0] for elem in c.fetchall()];
-
-print getGrades("kruder")
+    c.execute(query, (student,))
+    return [elem[0] for elem in c];
 
 # compute the average for each student
 def getAverage(student):
     grades = getGrades(student);
     total = 0;
-    numGrades = 0;
     for grade in grades:
         total+=grade
-        numGrades+=1
 
-    return total/numGrades
+    return total/float(len(grades))
 
-print getAverage("kruder")
+# display each student's name, id and average
+def printAverages():
+    query = "SELECT name, id FROM students"
+    d.execute(query)
+    for value in d:
+        print "Name: %s , Id: %d , Average: %f" %(value[0],value[1],getAverage(value[0]))
+
+printAverages()
